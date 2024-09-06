b52e98a5e9c4e185f761f484b74dc9453daa3b3c
qwerty111i <adwaithsramesh@gmail.com>
2024-09-06 00:03:45 -0400

    Changed heading + edited about text

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+document.title = 'Adwaith - Portfolio';
 root.render(
   <React.StrictMode>
     <App />
